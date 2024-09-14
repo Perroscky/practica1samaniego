93e8bbf84937b616b724f71d7e478eea4fe49702
Perroscky <la.samaniegos@uea.edu.ec>
2024-09-14 08:21:36 -0500

    Merge remote-tracking branch 'origin/main'

--- Desarrollo de Función para Calcular Temperaturas Promedio en Python/Funcion.py
+++ Desarrollo de Función para Calcular Temperaturas Promedio en Python/Funcion.py
@@ -1,13 +1,16 @@
 def calcular_promedio_temperaturas(temperaturas_esmeraldas, temperaturas_guayaquil, temperaturas_machala):
+#Esta línea define una nueva función llamada, esta función tomará tres listas de temperaturas como entrada, una para cada ciudad.
 
   # CANTIDAD DE SEMANAS
   if len(temperaturas_esmeraldas) != 4 or len(temperaturas_guayaquil) != 4 or len(temperaturas_machala) != 4:
     raise ValueError
+#Aquí se verifica que cada lista de temperaturas tenga exactamente 4 elementos
 
   # CALCULO DE PROMEDIOS
   promedio_Esmeraldas = sum(temperaturas_Esmeraldas) / len(temperaturas_Esmeraldas)
   promedio_Guayaquil = sum(temperaturas_Guayaquil) / len(temperaturas_Guayaquil)
   promedio_Machala = sum(temperaturas_Machala) / len(temperaturas_Machala)
+#Aqui cuanta los elementos, suma y divide para sacar el promedio
 
   # RETORNO DE RESULTADOS
   return {
@@ -15,11 +18,13 @@ def calcular_promedio_temperaturas(temperaturas_esmeraldas, temperaturas_guayaqu
     "Guayaquil": promedio_Guayaquil,
     "Machala": promedio_Machala
   }
+#Aqui la función devuelve un diccionario donde las claves son los nombres de las ciudades y los valores son los promedios de temperatura calculados
 
 # TEMPERATURAS
 temperaturas_Esmeraldas = [30, 32, 29, 28]
 temperaturas_Guayaquil = [34, 30, 33, 35]
 temperaturas_Machala = [29, 32, 28, 34]
+#Aqui se detalla las temperaturas de las 4 semanas
 
 # IMPRIME PROMEDIO DE TEMPERATURA DURANTE 4 SEMANAS
 promedios = calcular_promedio_temperaturas(temperaturas_Esmeraldas, temperaturas_Guayaquil, temperaturas_Machala)
